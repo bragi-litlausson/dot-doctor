366ca19c05565293391b0de566baff2aadffe081
Bragi Litlausson <bragi.litlausson@protonmail.com>
2021-09-01 01:24:58 +0200

    rewritten install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env sh
 path=$(dirname $0)
 dot_doctor="/dotdoctor.py"
 symlinkpath="/usr/local/bin/dotdoctor"
-ln -s $path$dot_doctor
-echo $path$dot_doctor
-echo
+echo "$path $dot_doctor"
+echo "$symlinkpath"
+ln -s $path$dot_doctor $symlinkpath
 chmod +x "/usr/local/bin/dotdoctor"
